a6a5ef6e5615ea20bdc14f273141cb78264bdc72
xujn <1170182824@qq.com>
2017-09-29 13:42:19 +0800

    python

--- testfile/test1/re.py
+++ testfile/test1/re.py
@@ -4,7 +4,8 @@
 # @Last Modified by:   xujn
 # @Last Modified time: 2017-09-29 11:21:26
 import sys
-sys.path.append('..')   #将上级目录添加到sys path中
+
+sys.path.append('..')  # 将上级目录添加到sys path中
 print(sys.path)
 import test2.fr
 
